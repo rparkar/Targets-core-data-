3f071e7e608c15ca4f16b017c4ba03bce3a2ac6a
Rehan Parkar <parkarcamp@gmail.com>
2018-04-27 18:06:00 -0500

    create target VC UI screen

---
+++ PostIT/controller/TargetViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TargetViewController.swift
+//  PostIT
+//
+//  Created by Rehan Parkar on 2018-04-27.
+//  Copyright © 2018 Rehan Parkar. All rights reserved.
+//
+
+import UIKit
+
+class TargetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
